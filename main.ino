2904d6f81c68c8dfee749e7606daab6cdae726b6
Flo <florent.garrit@gmail.com>
2018-11-12 09:48:03 +0100

    capteur lumiere ok

---
+++ main.ino
@@ -0,0 +1,16 @@
+#define lum 10 
+
+
+int void lecture_lum(void){
+ valeur_lum = analogRead(lum);
+ return lum;
+}
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
