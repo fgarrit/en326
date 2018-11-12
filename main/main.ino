16af3ccf09a10c1869e292995204d9f97c5b4e56
Flo <florent.garrit@gmail.com>
2018-11-12 10:21:01 +0100

    ajout main

--- main/main.ino
+++ main/main.ino
@@ -1,16 +1,50 @@
-#define lum 10 
+#define lum A0 
+#define dht11 2
 
+float lecture_dht11(int state){
+   String code = "INIT";
+   
+   switch (code)
+   { 
+    case INIT: 
+                Serial.println("INIT"); 
+                digitalWrite(dht11, HIGH);
+                if (state) case = "SINGLEBUS"
+                break;
+                
+   case SINGLEBUS: 
+                Serial.println("SINGLEBUS"); 
+                digitalWrite(dht11, LOW);
+                delay(20);
+                case = "PULLUP"
+                break;
+                
+   case PULLUP: 
+                Serial.println("PULLUP"); 
+                digitalWrite(dht11, LOW);
+                delay(20);
+                case = "PULLUP"
+                break;
 
-int void lecture_lum(void){
- valeur_lum = analogRead(lum);
- return lum;
+    default: 
+                Serial.println("Erreur inconnue"); 
+                break;
+   }
 }
-void setup() {
-  // put your setup code here, to run once:
 
+float lecture_lum(void){
+  float val_lum;
+  val_lum = 1/0.05*analogRead(lum)*5/1023; //Conversion    
+  return val_lum;
+}
+
+void setup() {
+Serial.begin(9600);   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  float lumiere;
+  lumiere = lecture_lum();
+  Serial.print(lumiere);
+  Serial.print("\n");
 }
